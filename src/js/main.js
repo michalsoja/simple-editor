de846c6d525830a0ad1e51a16a0386029e206497
Michal Soja <michal.soya@gmail.com>
2019-07-16 22:35:36 +0200

    feat:variables save load textarea

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
-"use strict";
-
-console.log(`Hello world!`);
+const btnSave = document.querySelector('.form__button--save-js');
+const btnLoad = document.querySelector('.form__button--load-js');
+const textarea = document.querySelector('.form__textarea-js');
