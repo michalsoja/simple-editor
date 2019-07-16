bf67fb6f4ec287968650c83092a87275c3983aae
Michal Soja <michal.soya@gmail.com>
2019-07-16 22:40:42 +0200

    feat:addEventListener for save load and textarea

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 const btnSave = document.querySelector('.form__button--save-js');
 const btnLoad = document.querySelector('.form__button--load-js');
 const textarea = document.querySelector('.form__textarea-js');
+
+btnSave.addEventListener('click', ()=>{
+    console.log('click')
+})
+
+btnLoad.addEventListener('click', ()=>{
+    console.log('click')
+})
+
+textarea.addEventListener('click', ()=>{
+    console.log('click')
+})
