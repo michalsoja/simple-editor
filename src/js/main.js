47ed660d8ceebef2e64160448b02855a8ed1914c
Michal Soja <michal.soya@gmail.com>
2019-07-17 23:22:17 +0200

    feat:added JS's fucntions

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
+import { loadavg } from "os";
+
 const btnSave = document.querySelector('.form__button--save-js');
 const btnLoad = document.querySelector('.form__button--load-js');
 const textarea = document.querySelector('.form__textarea-js');
 
-btnSave.addEventListener('click', ()=>{
-    console.log('click')
+btnSave.addEventListener('click', (e)=>{
+    e.preventDefault();
+    localStorage.setItem('itemValue', textarea.value);
 })
 
-btnLoad.addEventListener('click', ()=>{
-    console.log('click')
-})
-
-textarea.addEventListener('click', ()=>{
-    console.log('click')
+btnLoad.addEventListener('click', (e)=>{
+    e.preventDefault();
+    textarea.value = localStorage.getItem('itemValue');
 })
